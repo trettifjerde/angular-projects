378404d18daef0cd801cad2675cab7bab3ecb9a4
trettifjerde <trettifjerde@yandex.ru>
2022-07-06 10:49:06 +0200

    tour of heroes complete

--- projects/tour-of-heroes/src/app/app.module.ts
+++ projects/tour-of-heroes/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +19,18 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ projects/tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -8,16 +8,16 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  styleUrls: ['./hero-detail.component.scss'],
 })
 export class HeroDetailComponent implements OnInit {
-
   hero?: Hero;
 
   constructor(
     private route: ActivatedRoute,
     private location: Location,
-    private heroService: HeroService) { }
+    private heroService: HeroService
+  ) {}
 
   ngOnInit(): void {
     this.getHero();
@@ -25,11 +25,15 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  save(): void {
+    if (this.hero)
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
   }
 
   goBack(): void {
     this.location.back();
   }
-
 }
