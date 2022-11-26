5a7a26833ada136e607cd1ab49dd23d404a2e7ee
trettifjerde <sasjandreeva@gmail.com>
2022-11-26 20:26:45 +0100

    shopping: recipe detail and recipe form css updated

--- projects/shopping/src/app/app-router.module.ts
+++ projects/shopping/src/app/app-router.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, Route, RouterModule } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
+import { EmptyComponent } from "./shared/empty/empty.component";
+
 
 const appRoutes: Route[] = [
+    { path: '', pathMatch: 'full', redirectTo: '/recipes'},
     { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)},
     { path: 'list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)},
     { path: 'login', component: AuthComponent },
-    //{ path: '**', component: EmptyComponent, data: {message: 'Page not found'}}
+    { path: '**', component: EmptyComponent, data: {message: 'Page not found'}}
 ];
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
+    imports: [
+        RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
     exports: [RouterModule]
 })
 export class AppRouterModule {}
